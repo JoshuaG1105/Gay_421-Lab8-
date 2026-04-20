076f790fe77d7f0a291f5d2ee7d0b177629b6078
Joshua Gay <jjg6683@psu.edu>
2026-04-19 21:06:21 -0400

    Updated the wrong repository, just reverting the change

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const swaggerJSDoc = require('swagger-jsdoc');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 // --------------------
 // Middleware
 // --------------------
